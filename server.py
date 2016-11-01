dbd8dd9797e6d1bcc94159f0fc896fa3ca99ea74
Daniel Bok <daniel.bok@outlook.com>
2016-11-01 11:14:42 +0800

    changed server.py

--- server.py
+++ server.py
@@ -4,20 +4,20 @@ import cherrypy
 
 if __name__ == '__main__':
     app = create_app("server")
-
+    app.run()
     # Mount the application
-    cherrypy.tree.graft(app, "/")
+    # cherrypy.tree.graft(app, "/")
 
     # Unsubscribe the default server
-    cherrypy.server.unsubscribe()
+    # cherrypy.server.unsubscribe()
 
     # Instantiate a new server object
-    server = cherrypy._cpserver.Server()
+    # server = cherrypy._cpserver.Server()
 
     # Configure the server object
-    server.socket_host = "0.0.0.0"
-    server.socket_port = 5000
-    server.thread_pool = 30
+    # server.socket_host = "0.0.0.0"
+    # server.socket_port = 5000
+    # server.thread_pool = 30
 
     # For SSL Support
     # server.ssl_module            = 'pyopenssl'
@@ -26,9 +26,9 @@ if __name__ == '__main__':
     # server.ssl_certificate_chain = 'ssl/bundle.crt'
 
     # Subscribe this server
-    server.subscribe()
+    # server.subscribe()
 
     # Start the server engine (Option 1 *and* 2)
 
-    cherrypy.engine.start()
-    cherrypy.engine.block()
+    # cherrypy.engine.start()
+    # cherrypy.engine.block()
