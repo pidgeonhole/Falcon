3d3748b66637e7a9acbdad37bee11fad5872be5a
Daniel Bok <daniel.bok@outlook.com>
2016-11-07 15:18:08 +0800

    revert back server.py

--- server.py
+++ server.py
@@ -8,18 +8,18 @@ if __name__ == '__main__':
     # Mount the application
     cherrypy.tree.graft(app, "/")
 
-    cherrypy.config.update({
-        'server.socket_host'         : "0.0.0.0",
-        'server.socket_port'         : 5000,
-        'server.thread_pool'         : 30,
-        # 'server.ssl_module'           : 'pyopenssl',
-        # 'server.ssl_certificate'      : 'ssl/certificate.crt',
-        # 'server.ssl_private_key'      : 'ssl/private.key',
-        # 'server.ssl_certificate_chain': 'ssl/bundle.crt',
-        'engine.autoreload.on'       : False,
-        'engine.autoreload.frequency': 30,
-        'request.scheme'             : 'http'
-    })
+    # cherrypy.config.update({
+    #     'server.socket_host'         : "0.0.0.0",
+    #     'server.socket_port'         : 5000,
+    #     'server.thread_pool'         : 30,
+    #     # 'server.ssl_module'           : 'pyopenssl',
+    #     # 'server.ssl_certificate'      : 'ssl/certificate.crt',
+    #     # 'server.ssl_private_key'      : 'ssl/private.key',
+    #     # 'server.ssl_certificate_chain': 'ssl/bundle.crt',
+    #     'engine.autoreload.on'       : False,
+    #     'engine.autoreload.frequency': 30,
+    #     'request.scheme'             : 'http'
+    # })
 
     # Mount Static Directories
     cherrypy.tree.mount(None, '/static', config={
@@ -29,19 +29,19 @@ if __name__ == '__main__':
         }
     })
 
-    cherrypy.engine.start()
-    cherrypy.engine.block()
+    # cherrypy.engine.start()
+    # cherrypy.engine.block()
 
     # Unsubscribe the default server
-    # cherrypy.server.unsubscribe()
+    cherrypy.server.unsubscribe()
 
     # Instantiate a new server object
-    # server = cherrypy._cpserver.Server()
+    server = cherrypy._cpserver.Server()
 
     # Configure the server object
-    # server.socket_host = "0.0.0.0"
-    # server.socket_port = 5000
-    # server.thread_pool = 30
+    server.socket_host = "0.0.0.0"
+    server.socket_port = 5000
+    server.thread_pool = 30
 
     # For SSL Support
     # server.ssl_module            = 'pyopenssl'
@@ -50,8 +50,8 @@ if __name__ == '__main__':
     # server.ssl_certificate_chain = 'ssl/bundle.crt'
 
     # Subscribe this server
-    # server.subscribe()
+    server.subscribe()
 
     # Start the server engine (Option 1 *and* 2)
-    # cherrypy.engine.start()
-    # cherrypy.engine.block()
+    cherrypy.engine.start()
+    cherrypy.engine.block()
