a7a709f2b07e6061b60a9b7e9a0d933603c67e4a
Daniel Bok <daniel.bok@outlook.com>
2016-11-07 15:44:55 +0800

    updated prod settings

--- production/settings.py
+++ production/settings.py
@@ -1,7 +1,6 @@
 def __get_secret(key):
-    import os
-    return os.environ[key]
+    import os, random
+    return os.environ.get(key) or random.getrandbits(random.randint(120, 200))
 
 DEBUG = False
 SECRET_KEY = __get_secret("SECRET_KEY")
-
