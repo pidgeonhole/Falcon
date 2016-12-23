a451501da2e00805ef75be32cfd60616e93b2042
Daniel Bok <daniel.bok@outlook.com>
2016-12-23 21:59:35 +0800

    Fixed copy-hook bash command

--- deploy.sh
+++ deploy.sh
@@ -70,6 +70,8 @@ function run_application () {
 
 function copy_hook_file () {
     local DIRECTORY="/var/www/${APP_NAME}"
+    local SSH_SERVER="root@128.199.120.230"
+    echo "Copying Hook File"
     scp "${DEVOPS_FOLDER}/post-receive/${APP_NAME}" "${SSH_SERVER}:/tmp/${APP_NAME}"
     ssh -t "${SSH_SERVER}" bash -c "'
 sudo mv /tmp/${APP_NAME} ${DIRECTORY}.git/hooks/post-receive
